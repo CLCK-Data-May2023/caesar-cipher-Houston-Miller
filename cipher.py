669d23c26a1248a6ca88b9d93612411b9c74788f
Houston Miller <houston.miller93@gmail.com>
2023-08-06 00:02:43 -0400

    Attempting first push for grading

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
 # add your code here
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def encrypt(text):
+    ciphertext = ''
+    shift = 5
+    for character in text:
+        if character in alphabet:
+            position = alphabet.find(character)
+            new_position = (position + shift) % 26
+            new_character = alphabet[new_position]
+            ciphertext += new_character
+        else:
+            ciphertext += character
+    
+print("Enter Text to Encrypt")
+text = input()
+ciphertext = encrypt(text)
+print(ciphertext)
